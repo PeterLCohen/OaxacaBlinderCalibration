2736e80c50ecaa52014813a689969d6b3ee7ae04
PeterCohen <plcohen@mit.edu>
2020-11-27 13:54:57 -0500

    changing muhat to be a glm type

--- calibrateOB.R
+++ calibrateOB.R
@@ -1,15 +1,14 @@
-#' Calibration of Oaxaca blinder estimators 
+#' Calibration of Oaxaca blinder estimators
 #'
 #' Implements calibration method of Cohen & Fogarty (2020) to build non-inferior
 #'  Oaxaca-Blinder imputation estimators.
 #'
-#' @param muhatTreated imputation algorithm to use in the treated group
-#' @param muhatControl imputation algorithm to use in the treated group
+#' @param muhat model to use in the treated and control groups (must be a family of GLMs)
 #' @param Yobs the observed outcomes
 #' @param Z the observed treatment allocations
 #' @param X covariate matrix (each row is a single unit's covariates)
 #'
-#' @return estimate of sample average treatment effect 
+#' @return estimate of sample average treatment effect
 #'
 #' @export
 
@@ -19,5 +18,20 @@ calibrateOB = function(muhatTreated,
                        Z,
                        X)
 {
+  # Number of total units
+  N = length(Z)
+  
+  # Make sure that the treated and control models inherit from the lm class
+  # assertthat::assert_that('lm' %in% class(muhatTreated))
+  # assertthat::assert_that('lm' %in% class(muhatControl))
+  
+  # Make sure that Z is binary
+  assertthat::assert_that(sort(unique(Z)) == c(0, 1))
+  
+  # Make sure that dimensions of Yobs, X, and Z are compatible
+  assertthat::assert_that(length(Yobs) == N & dim(X)[1] == N)
+  
+  # Train the prediction functions muhatTreated and muhatControl
+  mu1 = glm(Yobs ~ .-Z, family=muhatTreated, subset(data, Z==1))
   
 }
